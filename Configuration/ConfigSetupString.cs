bab407ff312aca11d0826717e68e14ef8a792ebc
Zac Kilburn <kilburn.zachary@gmail.com>
2024-02-10 08:53:51 -0500

    Update ConfigSetupString.cs

--- Configuration/ConfigSetupString.cs
+++ Configuration/ConfigSetupString.cs
@@ -5,28 +5,32 @@ namespace ShipMaid.Configuration
 {
 	public class ConfigSetupString
 	{
+		public ConfigEntry<string> Key;
+
+		public string KeyDisplayName;
+
+		public string pluginName;
+
+		public string SettingDescription;
+
+		public string SettingName;
+
+		public string SettingValue;
+
 		public ConfigSetupString()
 		{
 		}
 
-		public string pluginName;
-		public string SettingName;
-		public string SettingValue;
-		public string SettingDescription;
+		public static ConfigEntry<string> CreateKey(ConfigSetupString c)
+		{
+			return ShipMaid.instance.Config.Bind(c.pluginName, c.SettingName, c.SettingValue, c.SettingDescription);
+		}
 
 		public Dictionary<string, ConfigEntryBase> Bind(Dictionary<string, ConfigEntryBase> toDictionary)
 		{
 			Key = CreateKey(this);
 			toDictionary.Add(Key.Definition.Key, (ConfigEntryBase)(object)Key);
 			return toDictionary;
-		}
-
-		public ConfigEntry<string> Key;
-		public string KeyDisplayName;
-
-		public static ConfigEntry<string> CreateKey(ConfigSetupString c)
-		{
-			return ShipMaid.instance.Config.Bind(c.pluginName, c.SettingName, c.SettingValue, c.SettingDescription);
 		}
 	}
 }
